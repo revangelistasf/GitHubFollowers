ff04ae0c600d7a4d5d85da1d3f2496d8db6f3d67
Roberto Evangelista <robertoevangelistasf@gmail.com>
2022-07-12 22:39:21 +0100

    feat: add Follower and User model

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Roberto Evangelista on 12/07/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
